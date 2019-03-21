78b7ae76e590dd8364ee5eca4f396fe0a771d242
Eliza Khachatryan <elijbet@gmail.com>
2019-03-20 20:31:35 -0700

    make DESIGN and CODE take us to respective pages

--- vue_portfolio/src/components/Landing.vue
+++ vue_portfolio/src/components/Landing.vue
@@ -2,13 +2,21 @@
 	<div class="background">
 		<div class="landing-flex font-2">
 			<div class="dimensions column-flex">
-				DESIGN
+				<router-link 
+					class="design-link"
+					:to="{ path: 'Design_Portfolio_Items' }">
+					DESIGN
+				</router-link>
 				<div class="font-1">
 					"Life is short, art is long." Hippocrates
 				</div>
 			</div>
 			<div class="dimensions column-flex">
-				{CODE}
+				<router-link 
+					class="design-link"
+					:to="{ path: 'Code_Portfolio_Items' }">
+					{CODE}
+				</router-link>
 				<div class="font-1">
 					"The best spice is hunger." Unknown
 				</div>
@@ -50,5 +58,9 @@
 	.column-flex {
 		display: flex;
 		flex-direction: column;
+	}
+	.design-link {
+		text-decoration: none;
+		color:  #2c3e50;
 	}
 </style>
