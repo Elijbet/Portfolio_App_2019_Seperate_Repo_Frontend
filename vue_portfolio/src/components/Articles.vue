97795d4ee91b771e483fff3989c7acace7307d81
Eliza Khachatryan <elijbet@gmail.com>
2019-03-26 17:35:22 -0700

    WIP: some work on the articles; need to figure out how to display images yet

--- vue_portfolio/src/components/Articles.vue
+++ vue_portfolio/src/components/Articles.vue
@@ -1,15 +1,31 @@
 <template>
- <div id="article">
- 	<router-link 
-		class="design-link"
-		:to="{ path: 'Add_Articles' }">
-		Add a new Blog Post
-	</router-link>
-  <div v-for="article in articles">
-   <p>{{article.title}}</p>
-   <p>{{article.text}}</p>
-  </div>
- </div>
+  <v-layout row>
+	  <v-flex
+        xs12
+        md6
+        lg4
+        v-for="article in articles"
+      >
+      <v-card class="card-style">
+        <img
+          :src="article.image.url"
+          height="200px">
+        <img>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">{{article.title}}</div>
+            <span class="grey--text">{{article.text}}</span>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat>Share</v-btn>
+          <v-btn flat color="purple">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+	  </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -21,17 +37,33 @@
 	   articles: [],
 	   title: [],
 	   text: [],
-	   errors: []
+	   errors: [],
 	  }
 	 },
-	 created() {
-	  axios.get('http://localhost:3000/articles') 
+	 created() {	  
+	  axios.get('http://localhost:3000/articles', {
+	    headers: {
+	      'Content-Type': 'application/json'
+	      // 'Content-Type': 'multipart/form-data'
+	    }
+	  })
 	  .then(response => {
-	   this.articles = response.data
+	  	console.log('response.data', response.data)
+	   	this.articles = response.data
+	   	console.log('this.articles[38].image.url', this.articles[37].image.url)
 	  })
 	 .catch(e => {
-	  this.error.push(e)
+	  // this.error.push(e)
 	  })
-	 },
+	 }
 	}
 </script>
+
+<style>
+	.layout.row {
+		flex-wrap: wrap;
+	}
+	.card-style {
+		margin: 10px;
+	}
+</style>
